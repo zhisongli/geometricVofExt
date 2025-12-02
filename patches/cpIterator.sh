a2944b015942b697acc025df5b50aa053a4c00a3
zhisongli <lizhisong@sjtu.edu.cn>
2025-12-02 23:21:05 +0800

    patch for Icx+IntelMPI

---
+++ patches/cpIterator.sh
@@ -0,0 +1,8 @@
+
+ofv=2506
+
+cp iterator.h ${HOME}/OpenFOAM/ThirdParty-v${ofv}/platforms/linux64Icx/CGAL-4.14.3/include/CGAL/boost/graph/
+
+cp CGAL ${HOME}/OpenFOAM/OpenFOAM-v${ofv}/wmake/rules/General/
+
+
